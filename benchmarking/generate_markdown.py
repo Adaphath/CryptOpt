646a55e1e6b4d20f234dd85dfe1efbcf5b082e0e
Sebastian Lindackers <sebastian.lindackers@gmail.com>
2024-02-14 07:33:04 +1030

    Add: markdown for all runs

--- benchmarking/generate_markdown.py
+++ benchmarking/generate_markdown.py
@@ -3,7 +3,6 @@ import json
 import os
 import mdutils
 
-mdFile = mdutils.MdUtils(file_name='benchmarking/results.md', title='Results')
 
 # Structure:
 # Heading 1: Curve Name
@@ -11,6 +10,9 @@ mdFile = mdutils.MdUtils(file_name='benchmarking/results.md', title='Results')
 # Comparison Plot
 # Heading 2: Statistical Tests (Mann-Whitney U Test (with Link to Wikipedia), Cohen's d (with Link to Wikipedia))
 # Table with p-values and effect sizes (for LS_SA_FIXED and LS_SA_THRESHOLD)
+
+fileName = "results.md"
+mdFile = mdutils.MdUtils(file_name='benchmarking/{}'.format(fileName), title='Results')
 
 curves = ["curve25519", "p256", "p384"]
 methods = ["mul", "square"]
@@ -20,21 +22,39 @@ for curve in curves:
   mdFile.new_header(level=1, title=curve)
   for method in methods:
     mdFile.new_header(level=2, title=method)
-    image_path = f"./plots/comparison_{curve}_{method}.png"
-    mdFile.new_line(mdFile.new_inline_image(text=f"Comparison Plot for {curve} {method}", path=image_path))
+    mdFile.new_header(level=2, title=f"{method} (all runs)")
+    image_path = f"./plots/comparison_{curve}_{method}.svg"
+    image_path_allRuns = f"./plots/comparison_{curve}_{method}_allRuns.svg"
+      
+    # mdFile.new_line(mdFile.new_inline_image(text=f"Comparison Plot for {curve} {method}", path=image_path))
+    # mdFile.new_line(mdFile.new_inline_image(text=f"Comparison Plot for {curve} {method} (all runs)", path=image_path_allRuns))
 
     mdFile.new_header(level=2, title="[Mann-Whitney U Test](https://en.wikipedia.org/wiki/Mann%E2%80%93Whitney_U_test) and [Cohen's d](https://en.wikipedia.org/wiki/Effect_size#Cohen's_d)")
     for evaluation in evaluations:
       # load data from json file
       effectSizeInputData = []
-      effectSizeFilePath = os.path.join(os.path.dirname(__file__), f"./statisticalTests/effectsize_{curve}_{method}.json")
+      effectSizeFileName = f"effectsize_{curve}_{method}.json"
+      effectSizeAllRunsFileName = f"effectsize_{curve}_{method}_allRuns.json"
+        
+      effectSizeFilePath = os.path.join(os.path.dirname(__file__), 'statisticalTests', effectSizeFileName)
+      effectSizeAllRunsFilePath = os.path.join(os.path.dirname(__file__), 'statisticalTests', effectSizeAllRunsFileName)
       with open(effectSizeFilePath) as f:
         effectSizeInputData = json.load(f)
+        
+      with open(effectSizeAllRunsFilePath) as f:
+        effectSizeInputDataAllRuns = json.load(f)
 
       mannWhitneyInputData = []
-      mannWhitneyFilePath = os.path.join(os.path.dirname(__file__), f"./statisticalTests/mannwhitneyu_{curve}_{method}.json")
+      mannWhitneyFileName = f"mannwhitneyu_{curve}_{method}.json"
+      mannWhitneyAllRunsFileName = f"mannwhitneyu_{curve}_{method}_allRuns.json"
+        
+      mannWhitneyFilePath = os.path.join(os.path.dirname(__file__), 'statisticalTests', mannWhitneyFileName)
+      mannWhitneyAllRunsFilePath = os.path.join(os.path.dirname(__file__), 'statisticalTests', mannWhitneyAllRunsFileName)
       with open(mannWhitneyFilePath) as f:
         mannWhitneyInputData = json.load(f)
+        
+      with open(mannWhitneyAllRunsFilePath) as f:
+        mannWhitneyInputDataAllRuns = json.load(f)
 
       print("evaluation: ", evaluation)
       print("effectSizeInputData: ", effectSizeInputData[evaluation])
@@ -51,6 +71,18 @@ for curve in curves:
       
       if mannWhitneyInputData[evaluation]["LS_SA_THRESHOLD"]["pValue"] is None:
         mannWhitneyInputData[evaluation]["LS_SA_THRESHOLD"]["pValue"] = 0
+        
+      if effectSizeInputDataAllRuns[evaluation]["LS_SA_FIXED"]["d"] is None:
+        effectSizeInputDataAllRuns[evaluation]["LS_SA_FIXED"]["d"] = 0
+      
+      if effectSizeInputDataAllRuns[evaluation]["LS_SA_THRESHOLD"]["d"] is None:
+        effectSizeInputDataAllRuns[evaluation]["LS_SA_THRESHOLD"]["d"] = 0
+        
+      if mannWhitneyInputDataAllRuns[evaluation]["LS_SA_FIXED"]["pValue"] is None:
+        mannWhitneyInputDataAllRuns[evaluation]["LS_SA_FIXED"]["pValue"] = 0
+        
+      if mannWhitneyInputDataAllRuns[evaluation]["LS_SA_THRESHOLD"]["pValue"] is None:
+        mannWhitneyInputDataAllRuns[evaluation]["LS_SA_THRESHOLD"]["pValue"] = 0
 
       mdFile.new_header(level=3, title=f"{evaluation}")
       # create table with p-values and effect sizes
@@ -60,5 +92,14 @@ for curve in curves:
       mdFile.new_line()
       mdFile.new_table(columns=3, rows=3, text=list_of_strings, text_align='center')
       
+      mdFile.new_header(level=3, title=f"{evaluation} (all runs)")
+      # create table with p-values and effect sizes
+      list_of_strings = ["Configuration", "p-value", "Effect Size"]
+      list_of_strings.extend(["LS_SA_FIXED", round(mannWhitneyInputDataAllRuns[evaluation]["LS_SA_FIXED"]["pValue"], 3), round(effectSizeInputDataAllRuns[evaluation]["LS_SA_FIXED"]["d"], 3)])
+      list_of_strings.extend(["LS_SA_THRESHOLD", round(mannWhitneyInputDataAllRuns[evaluation]["LS_SA_THRESHOLD"]["pValue"], 3), round(effectSizeInputDataAllRuns[evaluation]["LS_SA_THRESHOLD"]["d"], 3)])
+      mdFile.new_line()
+      mdFile.new_table(columns=3, rows=3, text=list_of_strings, text_align='center')
       
+# Create a table of contents
+mdFile.new_table_of_contents(table_title='Contents', depth=1)
 mdFile.create_md_file()
